3e1c0d64178c04defbf1aedc9ffcd292da5f5d98
Elias Norberg <xyzzy@kudzu.se>
2014-11-20 23:50:35 +0100

    Implement polling

--- config.py
+++ config.py
@@ -14,6 +14,12 @@ IMPORT_REQUIREMENTS=True
 REPOSITORY_PATH="."
 VIRTENV_PATH=".virtualenvs"
 
+# If we're polling, specify from where
+POLL_REPOSITORY_URL = "https://github.com/totalorder/django-photologue.git"
+#https://github.com/yzzyx/photoshop.git"
+POLL_REPOSITORY_NAME = "django-photologue"
+POLL_SLEEP = 3600
+
 # Use SQLITE3 for cache of branch test-info
 USE_SQLITE3 = True
 SQLITE3_DB = "testdata.db"
@@ -21,3 +27,5 @@ SQLITE3_DB = "testdata.db"
 # Template settings
 TEMPLATE_FILE="template.html"
 DATETIME_STR="%a, %d %b %Y %H:%M:%S"
+
+OUTPUT_FILE="test-stats.html"
---
+++ poll.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import config
+import json
+import os
+import handlers.handler
+import template
+import time
+from repo import Repository
+
+if config.POLL_REPOSITORY_URL == "":
+    print "You must set POLL_REPOSITORY_URL in config.py"
+
+if not os.path.isdir(config.VIRTENV_PATH):
+    os.makedirs(config.VIRTENV_PATH)
+
+clone_url = config.POLL_REPOSITORY_URL
+name = config.POLL_REPOSITORY_NAME
+
+variable_list = { "git_clone_url":  clone_url,
+                "git_repository_full_name": name,
+                "date": time.strftime(config.DATETIME_STR, time.localtime()) }
+
+repo = Repository(name)
+repo.set_clone_url(clone_url)
+repo.initialize()
+
+while 1:
+    repo.setup_env()
+    repo.fetch()
+    branches = repo.get_branches()
+
+    for b in branches:
+
+        # Check if we already have this data
+        if not b.cached_data:
+            print "Not cached: %s %s" % (b.name, time.strftime(config.DATETIME_STR,time.localtime(b.last_updated)))
+            (rv, total, failed, output) = handlers.handler.process_handlers()
+            b.failed_tests = failed
+            b.total_tests = total
+            b.output_log = output
+            b.save()
+        else:
+            print "Cached: %s %s" % (b.name, time.strftime(config.DATETIME_STR,time.localtime(b.last_updated)))
+
+    repo.reset_env()
+
+    t = template.Template(config.TEMPLATE_FILE)
+    t.set_variables(variable_list)
+    for b in branches:
+        t.add_branch(b.__dict__)
+
+    with open(config.OUTPUT_FILE, "w") as fd:
+         fd.write(t.parse())
+
+    # Wait for next poll
+    time.sleep(config.POLL_SLEEP)
