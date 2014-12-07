15f706fe2c3c92ec42cdee7fcc751d0b43b37a94
Elias Norberg <xyzzy@kudzu.se>
2014-12-07 18:33:08 +0100

    Update default config values

--- config.py
+++ config.py
@@ -3,29 +3,19 @@
 # This must be specified when setting up the github webhook
 SECRET="my-secret"
 
-# Note that the version must be installed, and accessible via
-# /usr/bin/pythonX.Y
-PYTHON_VERSION="2.7"
-
-# Set to false to ignore requirements.txt
-IMPORT_REQUIREMENTS=True
-
 # Where to store repos
-REPOSITORY_PATH="."
-VIRTENV_PATH=".virtualenvs"
+REPOSITORY_PATH=".repos"
 
 # If we're polling, specify from where
-POLL_REPOSITORY_URL = "https://github.com/totalorder/django-photologue.git"
-#https://github.com/yzzyx/photoshop.git"
-POLL_REPOSITORY_NAME = "django-photologue"
-POLL_SLEEP = 3600
+POLL_REPOSITORY_URL = "~/devel/test_repo"
+POLL_REPOSITORY_NAME = "test_repo"
+POLL_SLEEP = 15
 
 # Use SQLITE3 for cache of branch test-info
 USE_SQLITE3 = True
-SQLITE3_DB = "testdata.db"
+SQLITE3_DB = ".testfest.db"
 
 # Template settings
-TEMPLATE_FILE="template.html"
 DATETIME_STR="%a, %d %b %Y %H:%M:%S"
 
 OUTPUT_FILE="test-stats.html"
